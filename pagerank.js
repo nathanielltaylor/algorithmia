ed1ff5ae3c654bec131b571eea505cd34658302c
nathanielltaylor <ntaylor390@gmail.com>
2015-11-06 19:12:16 -0500

    pagerank

---
+++ pagerank.js
@@ -0,0 +1,12 @@
+// Get page rankings for my tweetmaps app
+var algorithmia = require("algorithmia");
+var client = algorithmia(process.env.ALGORITHMIA_API_KEY);
+var input = 'https://tweetmaps.herokuapp.com/';
+
+client.algo("/web/PageRank/0.1.0").pipe(input).then(function(output) {
+    if (output.error) {
+      console.log(output.error);
+    } else {
+      console.log(output.result);
+    }
+});
